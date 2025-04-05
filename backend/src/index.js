1a75f952200246bff5e9cd6f74dbce417f85e289
TausifAli2000 <tausifali2000@gmail.com>
2025-04-05 19:41:14 +0530

    express v change

--- backend/src/index.js
+++ backend/src/index.js
@@ -71,14 +71,11 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	const distPath = path.join(__dirname, "../frontend/dist");
-	app.use(express.static(distPath));
-
-	app.get("/*", (req, res) => {
-		res.sendFile(path.join(distPath, "index.html"));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
-
 
 // error handler
 app.use((err, req, res, next) => {
